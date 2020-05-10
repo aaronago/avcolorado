5d091203aa5da99bc13b9df90b60c57cf9cb7291
Aaron Gottlieb <atticus19@gmail.com>
2020-05-10 08:01:06 -0600

    Update READEME

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { FaFacebook, FaTwitterSquare, FaSquarespace } from 'react-icons/fa'
+import { FaFacebook } from 'react-icons/fa'
 
 export default [
   {
     icon: <FaFacebook />,
     url: 'https://www.facebook.com/anonymousforthevoiceless/',
   },
-  // {
-  //   icon: <FaTwitterSquare />,
-  //   url: "https://twitter.com",
-  // },
-  // {
-  //   icon: <FaSquarespace />,
-  //   url: "https://twitter.com",
-  // },
 ]
